dcdf15402beb50ed1979457474d47afd20943366
sandhiyasree <sandhiya413@gmail.com>
2018-03-11 13:20:31 +0530

    welcome blade modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome to Single Page Application</div>
+                <div class="title">Welcome to Single Page Application by KnowTrichy</div>
             </div>
         </div>
     </body>
